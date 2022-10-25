30861737f0be2da36f7aed00a3d8d087b1ccee78
sumiQAQ <80933462+sumiQAQ@users.noreply.github.com>
2022-10-26 00:21:41 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+
+namespace mainapp
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs e)
+        {
+            string _resName = MethodBase.GetCurrentMethod().DeclaringType.Namespace + ".libs." + new AssemblyName(e.Name).Name + ".dll";
+            using (var _stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(_resName))
+            {
+                if (_stream == null)
+                {
+                    return null;
+                }
+                byte[] _data = new byte[_stream.Length];
+                _stream.Read(_data, 0, _data.Length);
+                return Assembly.Load(_data);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using mainapp.Base_Class;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace mainapp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<sidebarclass> sidebarlist;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            sidebar_Init();
+        }
+
+        public void sidebar_Init()
+        {
+            Random ran = new Random();
+            sidebarlist = new List<sidebarclass>();
+            for (int i = 0; i < 5; i++)
+            {
+                sidebarlist.Add(new sidebarclass()
+                {
+                    name = i.ToString(),
+                    _id = ran.Next(10000,20000)
+                }) ;
+            }
+            //Sidebarclasses = new sidebarclass();
+            //Sidebarclasses.name = "164641";
+            BarMain.ItemsSource = sidebarlist;
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void mainD_Click(object sender, RoutedEventArgs e)
+        {
+            Sidebar.IsOpen = true;
+        }
+
+        private void BarMain_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+            testtext.Text = ((sidebarclass)BarMain.SelectedItem).name + '_' + ((sidebarclass)BarMain.SelectedItem)._id;
+
+        }
+    }
+}
